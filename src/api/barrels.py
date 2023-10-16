9a84b825d6c1eb5ed535f20178a548637dbd150d
Arne <arnenoori@gmail.com>
2023-10-16 13:52:57 -0700

    i am losing it

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -50,23 +50,19 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
         gold = gold_result.first().gold
         print(f"Gold: {gold}") 
 
+        # Fetch the current inventory
+        inventory_query = "SELECT * FROM inventory;"
+        inventory_result = connection.execute(sqlalchemy.text(inventory_query))
+        inventory = inventory_result.fetchall()
+
         for barrel in wholesale_catalog:
-            if gold > barrel.price:
+            # Check if we need this potion type
+            if inventory[barrel.potion_type.index(1)] < 10 and gold >= barrel.price:
                 print(f"Purchasing Barrel: {barrel.sku}")  # trying to see if I'm buying anything
                 purchase_plan.append({
                     "sku": barrel.sku,
                     "quantity": 1,
                 })
-                for i, potion_type in enumerate(barrel.potion_type):
-                    potion_ml = barrel.ml_per_barrel * barrel.quantity
-                    sql_query = f"""
-                    INSERT INTO inventory_transactions (description)
-                    VALUES ('Delivered {barrel.quantity} barrels of potion {potion_type}');
-
-                    INSERT INTO inventory_ledger_entries (inventory_id, transaction_id, change)
-                    VALUES ({potion_type}, (SELECT id FROM inventory_transactions ORDER BY id DESC LIMIT 1), {potion_ml});
-                    """
-                    connection.execute(sqlalchemy.text(sql_query))
                 gold -= barrel.price  # Update the gold amount
 
     return purchase_plan
