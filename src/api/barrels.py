d367c56a4b983d9ed9657534edde0d604d342532
Arne <arnenoori@gmail.com>
2023-10-16 21:46:53 -0700

    IM STRUGGLING AHHHHHH

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -40,52 +40,50 @@ def post_deliver_barrels(barrels_delivered: list[Barrel]):
 
 # Gets called once a day
 @router.post("/plan")
-# Works
 def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
-    purchase_plan = []
+    # Gold value and potion quantities from global_inventory
     with db.engine.begin() as connection:
+        sql_query = """SELECT gold, num_red_ml, num_green_ml, num_blue_ml, num_dark_ml FROM global_inventory"""
+        inventory = connection.execute(sqlalchemy.text(sql_query)).first()
+        gold, red_ml, green_ml, blue_ml, dark_ml = inventory
+
+    wholesale_catalog = sorted(wholesale_catalog, key=lambda x: x.ml_per_barrel)
+
+    res = {}
+    # always buy minis
+    def buy_potion(potion_type):
         for barrel in wholesale_catalog:
-            sql_query = f"""SELECT gold, num_red_ml, num_green_ml, num_blue_ml, num_dark_ml FROM global_inventory"""
-            result = connection.execute(sqlalchemy.text(sql_query))
-            inventory = result.first()
+            if barrel.potion_type == potion_type and gold >= barrel.price:
+                if barrel.sku in res:
+                    res[barrel.sku]["quantity"] += 1
+                else:
+                    res[barrel.sku] = {"quantity": 1}
+                gold -= barrel.price
+                break
 
-            if inventory is None:
-                return {
-                    "gold": 0,
-                    "num_red_ml": 0,
-                    "num_green_ml": 0,
-                    "num_blue_ml": 0,
-                    "num_dark_ml": 0
-                }
+    if red_ml < 100:
+        buy_potion([1, 0, 0, 0])
+    if green_ml < 100:
+        buy_potion([0, 1, 0, 0])
+    if blue_ml < 100:
+        buy_potion([0, 0, 1, 0])
 
-            gold, red_ml, green_ml, blue_ml, dark_ml = inventory
+    # Iterate through rest of barrels and buy if possible
+    for barrel in wholesale_catalog:
+        if barrel.price <= gold:
+            if barrel.sku in res:
+                res[barrel.sku]["quantity"] += 1
+            else:
+                res[barrel.sku] = {"quantity": 1}
+            gold -= barrel.price
 
-            if gold >= barrel.price:
-                purchase_plan.append({
-                    "sku": barrel.sku,
-                    "quantity": 1,
-                })
-                sql_query = f"""
-                UPDATE global_inventory
-                SET gold = gold - {barrel.price},
-                    num_red_ml = num_red_ml - {barrel.potion_type[0] * barrel.ml_per_barrel},
-                    num_green_ml = num_green_ml - {barrel.potion_type[1] * barrel.ml_per_barrel},
-                    num_blue_ml = num_blue_ml - {barrel.potion_type[2] * barrel.ml_per_barrel},
-                    num_dark_ml = num_dark_ml - {barrel.potion_type[3] * barrel.ml_per_barrel}
-                """
-                connection.execute(sqlalchemy.text(sql_query))
+    return res
 
-    return purchase_plan
-
-""""
-
-[Barrel(sku='SMALL_RED_BARREL', ml_per_barrel=500, potion_type=[1, 0, 0, 0], price=100, quantity=1) 
-Barrel(sku='MINI_RED_BARREL', ml_per_barrel=200, potion_type=[1, 0, 0, 0], price=60, quantity=1)
-
+'''
 Barrel(sku='SMALL_BLUE_BARREL', ml_per_barrel=500, potion_type=[0, 0, 1, 0], price=120, quantity=1)
 Barrel(sku='MINI_BLUE_BARREL', ml_per_barrel=200, potion_type=[0, 0, 1, 0], price=60, quantity=1)]
 
 Barrel(sku='SMALL_GREEN_BARREL', ml_per_barrel=500, potion_type=[0, 1, 0, 0], price=100, quantity=1) 
 Barrel(sku='MINI_GREEN_BARREL', ml_per_barrel=200, potion_type=[0, 1, 0, 0], price=60, quantity=1) 
     
-"""
+'''
