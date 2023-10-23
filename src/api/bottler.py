d03384f21b0ee914fbc7f4fed71b6dcfd4b53388
Arne <arnenoori@gmail.com>
2023-10-23 12:06:33 -0700

    spacing issue

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -6,6 +6,8 @@ from src.api import auth
 from src import database as db
 from sqlalchemy import text
 import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.pool import QueuePool
 
 router = APIRouter(
     prefix="/bottler",
@@ -76,8 +78,8 @@ def post_deliver_bottles(potions_delivered: List[PotionInventory]):
 
             print(f"Delivered potion: {potion}")
 
-        print("Finished delivery of potions.")
-        return "OK"
+    print("Finished delivery of potions.")
+    return "OK"
 
 @router.post("/plan")
 def get_bottle_plan():
@@ -96,48 +98,48 @@ def get_bottle_plan():
         """
         inventory = connection.execute(sqlalchemy.text(sql_query)).first()
 
-    if inventory is None:
-        red_ml, green_ml, blue_ml, dark_ml = 0, 0, 0, 0
-    else:
-        red_ml, green_ml, blue_ml, dark_ml = inventory
+        if inventory is None:
+            red_ml, green_ml, blue_ml, dark_ml = 0, 0, 0, 0
+        else:
+            red_ml, green_ml, blue_ml, dark_ml = inventory
 
-    # Query catalog for potion recipes
-    sql_query = """SELECT sku, name, quantity, price, num_red_ml, num_green_ml, num_blue_ml, num_dark_ml FROM catalog"""
-    catalog = connection.execute(sqlalchemy.text(sql_query)).fetchall()
+        # Query catalog for potion recipes
+        sql_query = """SELECT sku, name, quantity, price, num_red_ml, num_green_ml, num_blue_ml, num_dark_ml FROM catalog"""
+        catalog = connection.execute(sqlalchemy.text(sql_query)).fetchall()
 
-    for potion in catalog:
-        sku, name, quantity, price, required_red, required_green, required_blue, required_dark = potion
+        for potion in catalog:
+            sku, name, quantity, price, required_red, required_green, required_blue, required_dark = potion
 
-        # Check if we have enough ingredients for at least one potion
-        can_create = all([
-            red_ml >= required_red if required_red > 0 else True,
-            green_ml >= required_green if required_green > 0 else True,
-            blue_ml >= required_blue if required_blue > 0 else True,
-            dark_ml >= required_dark if required_dark > 0 else True
-        ])
+            # Check if we have enough ingredients for at least one potion
+            can_create = all([
+                red_ml >= required_red if required_red > 0 else True,
+                green_ml >= required_green if required_green > 0 else True,
+                blue_ml >= required_blue if required_blue > 0 else True,
+                dark_ml >= required_dark if required_dark > 0 else True
+            ])
 
-        if not can_create:
-            print(f"Not enough inventory to create even one '{name}'. Skipping...")
-            continue
+            if not can_create:
+                print(f"Not enough inventory to create even one '{name}'. Skipping...")
+                continue
 
-        # Determine the maximum number of potions we can create with the current inventory
-        max_potions = min(
-            (red_ml // required_red if required_red > 0 else MAX_SAME_POTION),
-            (green_ml // required_green if required_green > 0 else MAX_SAME_POTION),
-            (blue_ml // required_blue if required_blue > 0 else MAX_SAME_POTION),
-            (dark_ml // required_dark if required_dark > 0 else MAX_SAME_POTION),
-            MAX_SAME_POTION
-        )
-        print(f"Maximum of {max_potions} '{name}' potions can be created based on current inventory.")
+            # Determine the maximum number of potions we can create with the current inventory
+            max_potions = min(
+                (red_ml // required_red if required_red > 0 else MAX_SAME_POTION),
+                (green_ml // required_green if required_green > 0 else MAX_SAME_POTION),
+                (blue_ml // required_blue if required_blue > 0 else MAX_SAME_POTION),
+                (dark_ml // required_dark if required_dark > 0 else MAX_SAME_POTION),
+                MAX_SAME_POTION
+            )
+            print(f"Maximum of {max_potions} '{name}' potions can be created based on current inventory.")
 
-        # If we have abundant resources, create more potions
-        potion_count = max_potions if any(inventory > RESOURCE_THRESHOLD for inventory in [red_ml, green_ml, blue_ml, dark_ml]) else 1
+            # If we have abundant resources, create more potions
+            potion_count = max_potions if any(inventory > RESOURCE_THRESHOLD for inventory in [red_ml, green_ml, blue_ml, dark_ml]) else 1
 
-        if potion_count > 0:
-            print(f"Planning to create {potion_count} of potion: {name}, SKU: {sku}")
+            if potion_count > 0:
+                print(f"Planning to create {potion_count} of potion: {name}, SKU: {sku}")
 
-            # Add to the bottle plan without actually updating the database
-            bottle_plan.append({"potion_type": [required_red, required_green, required_blue, required_dark], "quantity": potion_count})
+                # Add to the bottle plan without actually updating the database
+                bottle_plan.append({"potion_type": [required_red, required_green, required_blue, required_dark], "quantity": potion_count})
 
     print("Final bottle plan:", bottle_plan)
     return bottle_plan
