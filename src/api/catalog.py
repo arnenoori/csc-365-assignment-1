2c0deb5d2e02bbb20b895053cb298dab18922bba
Arne <arnenoori@gmail.com>
2023-10-24 19:59:24 -0700

    catalog

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -14,7 +14,7 @@ def get_catalog():
         # Fetch the catalog from the database
         with db.engine.begin() as connection:
             sql_query = """
-                SELECT c.sku, c.name, SUM(ile.change), c.price, c.num_red_ml, c.num_green_ml, c.num_blue_ml, c.num_dark_ml 
+                SELECT c.sku, c.name, COALESCE(SUM(ile.change), 0), c.price, c.num_red_ml, c.num_green_ml, c.num_blue_ml, c.num_dark_ml 
                 FROM catalog c
                 LEFT JOIN inventory_ledger_entries ile ON c.id = ile.transaction_id
                 GROUP BY c.sku, c.name, c.price, c.num_red_ml, c.num_green_ml, c.num_blue_ml, c.num_dark_ml
